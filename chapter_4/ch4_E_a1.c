cc0fd71b257c15659f5d5e555e02eaec24d42448
Tamanna121 <tamannaag121@gmail.com>
2023-06-21 09:08:10 +0530

    a

--- chapter_4/ch4_E_a1.c
+++ chapter_4/ch4_E_a1.c
@@ -1,15 +1,24 @@
 //A) using conditional operators determine:
 //(1) whether the character entered through the keyboard is a lower case alphaet or not
+// #include<stdio.h>
+// int main()
+// {
+//     char a;
+//     printf("enter the character : ");
+//     scanf("%c",&a);
+//     if(a>=65 && a<=90)
+//         printf("the upper case");
+//             else
+//                 printf("lower case");
+
+//     return 0;
+// }
 #include<stdio.h>
 int main()
 {
     char a;
-    printf("enter the character : ");
+    printf("Enter the character : ");
     scanf("%c",&a);
-    if(a>=65 && a<=90)
-        printf("the lower case");
-            else
-                printf("not lower case");
-
+    (a>=65 && a<=90 ? printf("upper case"):printf("lower case"));
     return 0;
 }
